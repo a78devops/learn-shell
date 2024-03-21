50a38d892c41a86af3c925583b6a1422bbae2ede
pajjuriabhishek <pajjuriabhishek55@gmail.com>
2024-03-21 12:45:03 +0530

    functions

--- 04-functions.sh
+++ 04-functions.sh
@@ -7,11 +7,10 @@ example
 
 # functions have its own special variables
 
-example() {
+example1() {
   echo value of 1 - $1
   echo "value of * - $*"
   echo "value of # - $#"
 }
 
 example1 10 20 30
-example1 x y z
