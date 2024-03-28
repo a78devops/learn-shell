5686bdf0aaa5d4b1b74c02c5ccc262b32e6cc6e5
pajjuriabhishek <pajjuriabhishek55@gmail.com>
2024-03-28 11:54:42 +0530

    update code with DNS names

--- 02-vars.sh
+++ 02-vars.sh
@@ -7,3 +7,14 @@ b="hello world"
 
 echo $a
 echo $b
+
+# substitution
+# command sub
+# a command output if we want in a variable then command sub
+# syntax : var=$(command)
+x=$(uptime)
+
+# arithmetic sub
+# a arithmetic expression solved and output to a variable then command sub
+y=$((2+3))
+z=$((2+3-5/2*10))
